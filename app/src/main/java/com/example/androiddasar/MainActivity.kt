0d8ba870c7c55b57bb70d07fcc9c9ebf14b26a6b
DNRP Code <dnrpcode@gmail.com>
2023-05-27 21:15:49 +0700

    Move With Object Parcelable Parcelize

--- app/src/main/java/com/example/androiddasar/MainActivity.kt
+++ app/src/main/java/com/example/androiddasar/MainActivity.kt
@@ -19,6 +19,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
         btnMoveWithDataActivity.setOnClickListener(this)
+
+        val btnMoveWithObject:Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObject.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -33,6 +36,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 moveWithDataIntent.putExtra(MoveWIthDataActivity.EXTRA_NAME, "DicodingAcademy Boy")
                 moveWithDataIntent.putExtra(MoveWIthDataActivity.EXTRA_AGE, 5)
                 startActivity(moveWithDataIntent)
+            }
+
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                    "DicodingAcademy",
+                    5,
+                    "academy@dicoding.com",
+                    "Bandung"
+                )
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
             }
         }
     }
---
+++ app/src/main/java/com/example/androiddasar/Person.kt
@@ -0,0 +1,12 @@
+package com.example.androiddasar
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Person(
+    val name: String?,
+    val age: Int?,
+    val email: String?,
+    val city: String?
+): Parcelable
